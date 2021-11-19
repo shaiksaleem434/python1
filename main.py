26e1a4ed8f5c542d9bbac5f7baf7ff5419f54008
unknown <>
2021-11-19 14:28:44 +0530

    firstcommit

---
+++ main.py
@@ -0,0 +1,5 @@
+n=int(input())
+if s=s[::-1]:
+	print("pallendrome")
+else:
+	print("not a pallendrome")
---
+++ main1.py
@@ -0,0 +1,7 @@
+import math *
+n=int(input())
+t=sqrt(n)
+if t==n:
+	print("perfect square")
+else:
+	print("not perfect square")
